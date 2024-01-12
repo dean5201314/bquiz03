857ca4804d75df296d2db38a30d51d26db301a91
dean5201314 <smile90624@gmail.com>
2024-01-12 15:36:31 +0800

    建置預告片下方的按鈕內容

--- front/main_03.php
+++ front/main_03.php
@@ -1,6 +1,6 @@
 <style>
 .lists{
-   /*  position: relative; */
+    position: relative;
     left:114px;
 }
 .item *{
@@ -10,6 +10,7 @@
  width:200px;
  height:240px;
  margin:auto;
+ position:absolute;
  box-sizing: border-box;
  display:none;
 }
@@ -19,45 +20,7 @@
 }
 .item div {
     text-align: center;
-}
-.left,.right{
-    width:0;
-    border:20px solid black;
-    border-top-color:transparent;
-    border-bottom-color:transparent;  
-}
-.left{
-    border-left-width: 0;
-}
-.right{
-    border-right-width: 0;
-}
-.btns{
-    width:360px;
-    height:100px;
-    display:flex;
-    overflow: hidden;
-}
-
-.btn img{
-    width:60px;
-    height:80px;
-}
-.btn {
-    font-size:12px;
-    text-align: center;
-    flex-shrink:0;
-    width:90px;
-}
-.controls{
-    width:420px;
-    height:100px;
-    position: relative;
-    margin-top:10px;
-    display:flex;
-    align-items: center;
-    justify-content: space-between;
-}
+}    
 </style>
 <div class="half" style="vertical-align:top;">
     <h1>預告片介紹</h1>
@@ -65,7 +28,7 @@
         <div class="lists">
             <?php
             $posters=$Poster->all(['sh'=>1]," order by rank");
-            foreach($posters as $idx => $poster){
+            foreach($posters as $poster){
             ?>
             <div class="item">
                 <div><img src="./img/<?=$poster['img'];?>" alt=""></div>
@@ -77,25 +40,11 @@
         </div>
         
         <div class="controls">
-            <div class="left"></div>
-            <div class="btns">
-            <?php
-            foreach($posters as $idx =>$poster ){
-            ?>
-            <div class="btn">
-                <div><img src="./img/<?=$poster['img'];?>"></div>
-                <div><?=$poster['name'];?></div>
-            </div>
-            <?php 
-            }
-            ?>
-            </div>
-            <div class="right"></div>
         </div>
     </div>
 </div>
 <script>
-    $(".item").eq(0).show();
+$(".item").eq(0).show();
 
 
 </script>
